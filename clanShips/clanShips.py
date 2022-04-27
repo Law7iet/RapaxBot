8c5659e9d73b6f86ea6915a6b503d66056c93757
Han Chu <leonardo.chu@icloud.com>
2022-04-27 10:05:53 +0200

    Refactoring

--- clanShips/clanShips.py
+++ clanShips/clanShips.py
@@ -1,23 +1,124 @@
 from utils.apiWarGaming import ApiWarGaming
 from utils.constants import *
-from utils.functions import *
+
+
+def get_ships_table() -> dict:
+    """
+    Get an empty `dict` divided by ships tier.
+    Each tier has an own `dict` divided by type of ships.
+    Each type of ships is a `list`.
+
+    Returns:
+        `dict`: the empty structure to contains ships.
+    """
+    return {
+        str(int(ShipsTierEnum.ONE)): dict(
+            {
+                shipsType[ShipsTypeEnum.DESTROYER]: list(),
+                shipsType[ShipsTypeEnum.CRUISER]: list(),
+                shipsType[ShipsTypeEnum.BATTLESHIP]: list(),
+                shipsType[ShipsTypeEnum.AIR_CARRIER]: list()
+            }
+        ),
+        str(int(ShipsTierEnum.TWO)): dict(
+            {
+                shipsType[ShipsTypeEnum.DESTROYER]: list(),
+                shipsType[ShipsTypeEnum.CRUISER]: list(),
+                shipsType[ShipsTypeEnum.BATTLESHIP]: list(),
+                shipsType[ShipsTypeEnum.AIR_CARRIER]: list()
+            }
+        ),
+        str(int(ShipsTierEnum.THREE)): dict(
+            {
+                shipsType[ShipsTypeEnum.DESTROYER]: list(),
+                shipsType[ShipsTypeEnum.CRUISER]: list(),
+                shipsType[ShipsTypeEnum.BATTLESHIP]: list(),
+                shipsType[ShipsTypeEnum.AIR_CARRIER]: list()
+            }
+        ),
+        str(int(ShipsTierEnum.FOUR)): dict(
+            {
+                shipsType[ShipsTypeEnum.DESTROYER]: list(),
+                shipsType[ShipsTypeEnum.CRUISER]: list(),
+                shipsType[ShipsTypeEnum.BATTLESHIP]: list(),
+                shipsType[ShipsTypeEnum.AIR_CARRIER]: list()
+            }
+        ),
+        str(int(ShipsTierEnum.FIVE)): dict(
+            {
+                shipsType[ShipsTypeEnum.DESTROYER]: list(),
+                shipsType[ShipsTypeEnum.CRUISER]: list(),
+                shipsType[ShipsTypeEnum.BATTLESHIP]: list(),
+                shipsType[ShipsTypeEnum.AIR_CARRIER]: list()
+            }
+        ),
+        str(int(ShipsTierEnum.SIX)): dict(
+            {
+                shipsType[ShipsTypeEnum.DESTROYER]: list(),
+                shipsType[ShipsTypeEnum.CRUISER]: list(),
+                shipsType[ShipsTypeEnum.BATTLESHIP]: list(),
+                shipsType[ShipsTypeEnum.AIR_CARRIER]: list()
+            }
+        ),
+        str(int(ShipsTierEnum.SEVEN)): dict(
+            {
+                shipsType[ShipsTypeEnum.DESTROYER]: list(),
+                shipsType[ShipsTypeEnum.CRUISER]: list(),
+                shipsType[ShipsTypeEnum.BATTLESHIP]: list(),
+                shipsType[ShipsTypeEnum.AIR_CARRIER]: list()
+            }
+        ),
+        str(int(ShipsTierEnum.EIGHT)): dict(
+            {
+                shipsType[ShipsTypeEnum.DESTROYER]: list(),
+                shipsType[ShipsTypeEnum.CRUISER]: list(),
+                shipsType[ShipsTypeEnum.BATTLESHIP]: list(),
+                shipsType[ShipsTypeEnum.AIR_CARRIER]: list()
+            }
+        ),
+        str(int(ShipsTierEnum.NINE)): dict(
+            {
+                shipsType[ShipsTypeEnum.DESTROYER]: list(),
+                shipsType[ShipsTypeEnum.CRUISER]: list(),
+                shipsType[ShipsTypeEnum.BATTLESHIP]: list(),
+                shipsType[ShipsTypeEnum.AIR_CARRIER]: list()
+            }
+        ),
+        str(int(ShipsTierEnum.TEN)): dict(
+            {
+                shipsType[ShipsTypeEnum.DESTROYER]: list(),
+                shipsType[ShipsTypeEnum.CRUISER]: list(),
+                shipsType[ShipsTypeEnum.BATTLESHIP]: list(),
+                shipsType[ShipsTypeEnum.AIR_CARRIER]: list()
+            }
+        ),
+        str(int(ShipsTierEnum.ELEVEN)): dict(
+            {
+                shipsType[ShipsTypeEnum.DESTROYER]: list(),
+                shipsType[ShipsTypeEnum.CRUISER]: list(),
+                shipsType[ShipsTypeEnum.BATTLESHIP]: list(),
+                shipsType[ShipsTypeEnum.AIR_CARRIER]: list()
+            }
+        )
+    }
+
 
 class ClanShips:
     def __init__(self):
-        self.apiWarGaming = ApiWarGaming()
-        self.shipsById = {}
+        self.api_wargaming = ApiWarGaming()
+        self.ships_by_id = {}
         """
         It's a dictionary that stores all ships by their ID.
         The key is the ship ID and the value is a ship.
         Each ship is a dictionary that contains:
-            `name` (str)\n
-            `type` (ShipsTypeEnum)\n
-            `tier` (ShipsTierEnum)\n
-            `nation` (str)              not used\n
-            `is_special` (bool)         not used\n
-            `is_premium` (bool)         not used\n
-        """        
-        self.shipsByTierAndType = self.getShipsTable()
+            `name`          (str)\n
+            `type`          (ShipsTypeEnum)\n
+            `tier`          (ShipsTierEnum)\n
+            `nation`        (str)               not used\n
+            `is_special`    (bool)              not used\n
+            `is_premium`    (bool)              not used\n
+        """
+        self.ships_by_tier_and_type = get_ships_table()
         """
         It's a dictionary that stores all ships by tier and type.
         The key is the ships tier and the value is a sub-dictionary that keys are the ships' type.
@@ -25,125 +126,103 @@ class ClanShips:
         Each ships is a dictionary that contains:
             `id` (str)\n
             `name` (str)
-        """        
-        self.setAllShips() 
-
-    def getShipsTable(self) -> dict:
-        '''
-        Get an empty `dict` divided by ships tier.
-        Each tier has a own `dict` divided by type of ships.
-        Each type of ships is a `list`.
-
-        Returns:
-            `dict`: the empty structure to contains ships.
-        '''        
-        return  {
-            str(int(ShipsTierEnum.ONE))     : dict({ shipsType[ShipsTypeEnum.DESTROYER]: list(), shipsType[ShipsTypeEnum.CRUISER]: list(), shipsType[ShipsTypeEnum.BATTLESHIP]: list(), shipsType[ShipsTypeEnum.AIR_CARRIER]: list() }),
-            str(int(ShipsTierEnum.TWO))     : dict({ shipsType[ShipsTypeEnum.DESTROYER]: list(), shipsType[ShipsTypeEnum.CRUISER]: list(), shipsType[ShipsTypeEnum.BATTLESHIP]: list(), shipsType[ShipsTypeEnum.AIR_CARRIER]: list() }),
-            str(int(ShipsTierEnum.THREE))   : dict({ shipsType[ShipsTypeEnum.DESTROYER]: list(), shipsType[ShipsTypeEnum.CRUISER]: list(), shipsType[ShipsTypeEnum.BATTLESHIP]: list(), shipsType[ShipsTypeEnum.AIR_CARRIER]: list() }),
-            str(int(ShipsTierEnum.FOUR))    : dict({ shipsType[ShipsTypeEnum.DESTROYER]: list(), shipsType[ShipsTypeEnum.CRUISER]: list(), shipsType[ShipsTypeEnum.BATTLESHIP]: list(), shipsType[ShipsTypeEnum.AIR_CARRIER]: list() }),
-            str(int(ShipsTierEnum.FIVE))    : dict({ shipsType[ShipsTypeEnum.DESTROYER]: list(), shipsType[ShipsTypeEnum.CRUISER]: list(), shipsType[ShipsTypeEnum.BATTLESHIP]: list(), shipsType[ShipsTypeEnum.AIR_CARRIER]: list() }),
-            str(int(ShipsTierEnum.SIX))     : dict({ shipsType[ShipsTypeEnum.DESTROYER]: list(), shipsType[ShipsTypeEnum.CRUISER]: list(), shipsType[ShipsTypeEnum.BATTLESHIP]: list(), shipsType[ShipsTypeEnum.AIR_CARRIER]: list() }),
-            str(int(ShipsTierEnum.SEVEN))   : dict({ shipsType[ShipsTypeEnum.DESTROYER]: list(), shipsType[ShipsTypeEnum.CRUISER]: list(), shipsType[ShipsTypeEnum.BATTLESHIP]: list(), shipsType[ShipsTypeEnum.AIR_CARRIER]: list() }),
-            str(int(ShipsTierEnum.EIGHT))   : dict({ shipsType[ShipsTypeEnum.DESTROYER]: list(), shipsType[ShipsTypeEnum.CRUISER]: list(), shipsType[ShipsTypeEnum.BATTLESHIP]: list(), shipsType[ShipsTypeEnum.AIR_CARRIER]: list() }),
-            str(int(ShipsTierEnum.NINE))    : dict({ shipsType[ShipsTypeEnum.DESTROYER]: list(), shipsType[ShipsTypeEnum.CRUISER]: list(), shipsType[ShipsTypeEnum.BATTLESHIP]: list(), shipsType[ShipsTypeEnum.AIR_CARRIER]: list() }),
-            str(int(ShipsTierEnum.TEN))     : dict({ shipsType[ShipsTypeEnum.DESTROYER]: list(), shipsType[ShipsTypeEnum.CRUISER]: list(), shipsType[ShipsTypeEnum.BATTLESHIP]: list(), shipsType[ShipsTypeEnum.AIR_CARRIER]: list() }),
-            str(int(ShipsTierEnum.ELEVEN))  : dict({ shipsType[ShipsTypeEnum.DESTROYER]: list(), shipsType[ShipsTypeEnum.CRUISER]: list(), shipsType[ShipsTypeEnum.BATTLESHIP]: list(), shipsType[ShipsTypeEnum.AIR_CARRIER]: list() })
-        }
-
-    def setAllShips(self) -> None:
         """
-        Initialize `shipsById` and `shipsByTierAndType` dictionary.\n
+        self.set_all_ships()
+
+    def set_all_ships(self) -> None:
+        """
+        Initialize `ships_by_id` and `ships_by_tier_and_type` dictionary.\n
         
-        Each ships in `shipsByTierAndType` is a dictionary that contains:
-            `id` (str)\n
-            `name` (str)
+        Each ships in `ships_by_tier_and_type` is a dictionary that contains:
+            `id`    (str)\n
+            `name`  (str)
 
         Returns:
-            `None`
+            None
         """
-        responseShips = self.apiWarGaming.get_ships()
-        if responseShips == []:
+        response_ships = self.api_wargaming.get_ships()
+        if not response_ships:
             return None
-        for shipElement in responseShips:
-            shipId = shipElement[0]
-            shipElement = shipElement[1]
-            if shipElement['tier'] == 1:
+        for ship_element in response_ships:
+            ship_id = ship_element[0]
+            ship_element = ship_element[1]
+            if ship_element['tier'] == 1:
                 tier = ShipsTierEnum.ONE
-            elif shipElement['tier'] == 2:
+            elif ship_element['tier'] == 2:
                 tier = ShipsTierEnum.TWO
-            elif shipElement['tier'] == 3:
+            elif ship_element['tier'] == 3:
                 tier = ShipsTierEnum.THREE
-            elif shipElement['tier'] == 4:
+            elif ship_element['tier'] == 4:
                 tier = ShipsTierEnum.FOUR
-            elif shipElement['tier'] == 5:
+            elif ship_element['tier'] == 5:
                 tier = ShipsTierEnum.FIVE
-            elif shipElement['tier'] == 6:
+            elif ship_element['tier'] == 6:
                 tier = ShipsTierEnum.SIX
-            elif shipElement['tier'] == 7:
+            elif ship_element['tier'] == 7:
                 tier = ShipsTierEnum.SEVEN
-            elif shipElement['tier'] == 8:
+            elif ship_element['tier'] == 8:
                 tier = ShipsTierEnum.EIGHT
-            elif shipElement['tier'] == 9:
+            elif ship_element['tier'] == 9:
                 tier = ShipsTierEnum.NINE
-            elif shipElement['tier'] == 10:
+            elif ship_element['tier'] == 10:
                 tier = ShipsTierEnum.TEN
-            elif shipElement['tier'] == 11:
+            elif ship_element['tier'] == 11:
                 tier = ShipsTierEnum.ELEVEN
             else:
                 return None
-                
-            if shipElement['type'] == 'Destroyer':
-                type = ShipsTypeEnum.DESTROYER
-            elif shipElement['type'] == 'Cruiser':
-                type = ShipsTypeEnum.CRUISER
-            elif shipElement['type'] == 'Battleship':
-                type = ShipsTypeEnum.BATTLESHIP
-            elif shipElement['type'] == 'AirCarrier':
-                type = ShipsTypeEnum.AIR_CARRIER
-            elif shipElement['type'] == 'Submarine':
-                type = ShipsTypeEnum.SUBMARINE
+
+            if ship_element['type'] == 'Destroyer':
+                ships_type = ShipsTypeEnum.DESTROYER
+            elif ship_element['type'] == 'Cruiser':
+                ships_type = ShipsTypeEnum.CRUISER
+            elif ship_element['type'] == 'Battleship':
+                ships_type = ShipsTypeEnum.BATTLESHIP
+            elif ship_element['type'] == 'AirCarrier':
+                ships_type = ShipsTypeEnum.AIR_CARRIER
+            elif ship_element['type'] == 'Submarine':
+                ships_type = ShipsTypeEnum.SUBMARINE
             else:
                 return None
-            
+
             ship = {
-                'id': shipId,
-                'name': shipElement['name'],
-                'type': type,
+                'id': ship_id,
+                'name': ship_element['name'],
+                'type': ships_type,
                 'tier': tier,
-                'nation': shipElement['nation'],
-                'is_special': shipElement['is_special'],
-                'is_premium': shipElement['is_premium']
+                'nation': ship_element['nation'],
+                'is_special': ship_element['is_special'],
+                'is_premium': ship_element['is_premium']
             }
             # Remove "[ name ]" ship
-            if shipElement['name'][0] != '[':
-                self.shipsById[str(shipElement['ship_id'])] = ship
-                self.shipsByTierAndType[str(int(ship['tier']))][shipsType[int(ship['type'])]].append({ 'id': shipId, 'name': ship['name'] })
+            if ship_element['name'][0] != '[':
+                self.ships_by_id[str(ship_element['ship_id'])] = ship
+                self.ships_by_tier_and_type[str(int(ship['tier']))][shipsType[int(ship['type'])]].append(
+                    {'id': ship_id, 'name': ship['name']})
 
         # Sort and clear ships in all_ship_ordered
-        for shipTier in self.shipsByTierAndType:
-            for shipType in self.shipsByTierAndType[str(shipTier)]:
-                self.shipsByTierAndType[str(shipTier)][shipType].sort(key=lambda d: d['name'])
+        for ship_tier in self.ships_by_tier_and_type:
+            for ship_type in self.ships_by_tier_and_type[str(ship_tier)]:
+                self.ships_by_tier_and_type[str(ship_tier)][ship_type].sort(key=lambda d: d['name'])
                 index = 0
-                for ship in self.shipsByTierAndType[str(shipTier)][shipType]:
+                for ship in self.ships_by_tier_and_type[str(ship_tier)][ship_type]:
                     # Map the index of sorted ships in all_ships
-                    self.shipsById[ship['id']]['index'] = index
+                    self.ships_by_id[ship['id']]['index'] = index
                     index = index + 1
 
-    def getPlayerTable(self) -> dict:
+    def get_player_table(self) -> dict:
         """
-        Returns the `dict` `shipsByTierAndType` filled with `False` for each ship.
+        Returns the `dict` `ships_by_tier_and_type` filled with `False` for each ship.
 
         Returns:
-            `dict`: the table
-        """        
-        playerTable = self.getShipsTable()
-        for tierIndex in self.shipsByTierAndType:
-            for typeIndex in self.shipsByTierAndType[tierIndex]:
-                for shipIndex in range(0, len(self.shipsByTierAndType[tierIndex][typeIndex])):
-                    playerTable[tierIndex][typeIndex].append(False)
+            the table
+        """
+        player_table = get_ships_table()
+        for tier_index in self.ships_by_tier_and_type:
+            for type_index in self.ships_by_tier_and_type[tier_index]:
+                for ship_index in range(0, len(self.ships_by_tier_and_type[tier_index][type_index])):
+                    player_table[tier_index][type_index].append(False)
 
-        return playerTable
+        return player_table
 
     def getPlayersShips(self, clanId: int) -> dict:
         """
@@ -152,41 +231,41 @@ class ClanShips:
         This list is a boolean list and means if the player has a specific ships.
 
         Args:
-            clanId (int): the clan ID.
+            clanId: the clan ID.
 
         Returns:
-            `dict`: the dictionary with all players' ships.
-        """        
+            the dictionary with all players' ships.
+        """
         # returned dict
-        mappedList = {}
+        mapped_list = {}
         # Request clan's members' ID
-        responseClanDetails = self.apiWarGaming.get_clan_members(clanId)
-        if responseClanDetails == []:
-            return mappedList
+        response_clan_details = self.api_wargaming.get_clan_members(clanId)
+        if not response_clan_details:
+            return mapped_list
 
         # For each member
-        for id in responseClanDetails:
+        for member_id in response_clan_details:
             try:
-                response = self.apiWarGaming.get_player_by_id(str(id))
-                if response == None:
+                response = self.api_wargaming.get_player_by_id(str(member_id))
+                if response is None:
                     continue
-                playerNickname = response[1]
-                mappedShips = self.getPlayerTable()
+                player_nickname = response[1]
+                mapped_ships = self.get_player_table()
                 # Get member's ships
-                responseMemberShips = self.apiWarGaming.get_player_ships(str(id))
-                if responseMemberShips == []:
+                response_member_ships = self.api_wargaming.get_player_ships(str(member_id))
+                if not response_member_ships:
                     continue
                 # For each member's ship
-                for shipId in responseMemberShips:
-                    # Old ship has an id but is not in all_ships
+                for shipId in response_member_ships:
+                    # Old ship has a member_id but is not in all_ships
                     try:
                         # map the player's ship
-                        ship = self.shipsById[str(shipId)]
-                        mappedShips[str(int(ship['tier']))][shipsType[int(ship['type'])]][ship['index']] = True
+                        ship = self.ships_by_id[str(shipId)]
+                        mapped_ships[str(int(ship['tier']))][shipsType[int(ship['type'])]][ship['index']] = True
                     except:
                         pass
             except:
                 pass
-            
-            mappedList[playerNickname] = mappedShips
-        return mappedList
+
+            mapped_list[player_nickname] = mapped_ships
+        return mapped_list
